a333b9d1d78df50a72dee8bbf5747d3eaf963dc6
erfyo <sherfydak1ng@gmail.com>
2025-12-21 19:17:10 +0100

    Update erfyo_test.py

--- erfyo_test.py
+++ erfyo_test.py
@@ -4,4 +4,5 @@ st.set_page_config(page_title="Erfyo Portfolio", page_icon="🚀")
 
 st.title("Cześć! Tu Erfyo 👋")
 st.write("To moja pierwsza aplikacja postawiona na własnym VPS przez Dokploy!")
-st.code("print('Hello World from VPS!')", language="python")
+
+st.code("print('Hello World from VPS! SUPER!')", language="python")
